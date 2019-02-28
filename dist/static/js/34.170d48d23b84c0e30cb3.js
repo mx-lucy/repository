a881e0306c74f4ea791992c51e743be0d8704dc8
mx-lucy <lucy.chen@wh-mx.com>
2019-02-28 10:13:59 +0800

    dist修改

--- dist/static/js/34.170d48d23b84c0e30cb3.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([34],{"3zYO":function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var r={render:function(){var e=this.$createElement;return(this._self._c||e)("router-view")},staticRenderFns:[]};var u=n("VU/8")({name:"index"},r,!1,function(e){n("Su4h")},"data-v-03745f5e",null);t.default=u.exports},Su4h:function(e,t){}});
-//# sourceMappingURL=34.170d48d23b84c0e30cb3.js.map
